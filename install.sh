eb50d097eaf51010aa80516e817d3477b1d55904
Bill DeCoste <wdecoste@redhat.com>
2017-05-26 12:39:08 -0700

    update

--- install.sh
+++ install.sh
@@ -9,6 +9,6 @@ ${JBOSS_HOME}/bin/jboss-cli.sh --file=${injected_dir}/setup.cli
 mkdir -p ${JBOSS_HOME}/standalone/tmp/vfs/temp
 mkdir -p ${JBOSS_HOME}/standalone/data/content
 mkdir -p ${JBOSS_HOME}/standalone/data/activemq/journal
-chmod -R 777 ${JBOSS_HOME}/standalone/data/content
+chmod -R 777 ${JBOSS_HOME}/standalone/data
 chmod -R 777 ${JBOSS_HOME}/standalone/tmp
 chown -R jboss:root ${JBOSS_HOME}/standalone
