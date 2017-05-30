70e198e9583a3854937deacc19abb9ff0373e57e
Bill DeCoste <wdecoste@redhat.com>
2017-05-30 10:55:10 -0700

    update

--- install.sh
+++ install.sh
@@ -4,17 +4,17 @@ set -x
 
 injected_dir=$1
 
-defaultjobrepo="<default-job-repository name=\"in-memory\"/>"
+#defaultjobrepo="<default-job-repository name=\"in-memory\"/>"
 
-sed -i "s|<!-- ##DEFAULT_JOB_REPOSITORY## -->|${defaultjobrepo}" ${JBOSS_HOME}/standalone/configuation/standalone-openshift.xml
+#sed -i "s|<!-- ##DEFAULT_JOB_REPOSITORY## -->|${defaultjobrepo}" ${JBOSS_HOME}/standalone/configuation/standalone-openshift.xml
 
-${JBOSS_HOME}/bin/jboss-cli.sh --file=${injected_dir}/setup.cli
+#${JBOSS_HOME}/bin/jboss-cli.sh --file=${injected_dir}/setup.cli
 
-mkdir -p ${JBOSS_HOME}/standalone/tmp/vfs/temp
-mkdir -p ${JBOSS_HOME}/standalone/data/content
-mkdir -p ${JBOSS_HOME}/standalone/data/activemq/journal
-mkdir -p ${JBOSS_HOME}/standalone/configuration/standalone_xml_history
-chmod -R 777 ${JBOSS_HOME}/standalone/data
-chmod -R 777 ${JBOSS_HOME}/standalone/tmp
-chmod -R 777 ${JBOSS_HOME}/standalone/configuration/standalone_xml_history
-chown -R 185:root ${JBOSS_HOME}
+#mkdir -p ${JBOSS_HOME}/standalone/tmp/vfs/temp
+#mkdir -p ${JBOSS_HOME}/standalone/data/content
+#mkdir -p ${JBOSS_HOME}/standalone/data/activemq/journal
+#mkdir -p ${JBOSS_HOME}/standalone/configuration/standalone_xml_history
+#chmod -R 777 ${JBOSS_HOME}/standalone/data
+#chmod -R 777 ${JBOSS_HOME}/standalone/tmp
+#chmod -R 777 ${JBOSS_HOME}/standalone/configuration/standalone_xml_history
+#chown -R 185:root ${JBOSS_HOME}
